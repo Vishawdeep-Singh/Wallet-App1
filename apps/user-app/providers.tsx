9d2519432ed1ef95202ac95062087a7cead0a77d
Vishawdeep-Singh <vishawdeepsingh29@gmail.com>
2024-08-12 13:02:58 +0530

    test

--- apps/user-app/providers.tsx
+++ apps/user-app/providers.tsx
@@ -3,6 +3,7 @@ import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
 
 export const Providers = ({children}: {children: React.ReactNode}) => {
+    
     return <RecoilRoot>
         <SessionProvider>
         {children}
